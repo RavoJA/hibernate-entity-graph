eb702d4264de81dc4bd16ea1e0462b1d0a71812c
Jean Aimé <jeanaime.ravomanana@gmail.com>
2020-04-12 15:01:03 +0300

    initial

---
+++ src/main/java/com/jean/aime/hibernateentitygraph/HibernateEntityGraphApplication.java
@@ -0,0 +1,13 @@
+package com.jean.aime.hibernateentitygraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEntityGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateEntityGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jean/aime/hibernateentitygraph/HibernateEntityGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.aime.hibernateentitygraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEntityGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
